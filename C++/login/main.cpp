2ca29159a2b5fa30595d308d2d7700cc4c542f53
Clon54 <clon5454@outlook.com>
2023-02-11 19:26:48 -0500

    cree la clase de login y un metodo de verificacion

--- C++/login/main.cpp
+++ C++/login/main.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+class login{
+    public:
+    string Username;
+    string password;
+    bool config;
+     void Request();//aqui llama la funcion
+};
+
+void login::Request(){
+    login respuest;
+
+    if (respuest.password.length() >= 8)
+    {
+        cout<<"tu contraseña cumple las caracteristicas de seguridad recomendadas \n";
+        respuest.config = true;
+    }else{
+        cout<<"tu contraseña no cumple los requistos de seguridad";
+        respuest.config = false;
+    }
+    
+
+}
+
 int main(int argc, char const *argv[])
 {
+    login myObj;
     //como hacer un logn en c++
+    cout<<"deme su usuario y contraseña \n";
+    cout<<"deme primero su usuario"<<"\n";
+    cin>>myObj.Username;
+
+    cout<<"deme su contraseña \n";
+    cin>>myObj.password;
+
+    //la verificacion
+
     return 0;
 }
