9068943728f8e8900f0bd0ef16f3fc628dae6a2d
Clon54 <clon5454@outlook.com>
2023-02-15 20:16:42 -0500

    funciona pero el primer intento manda error

--- C++/login/otrologin.cpp
+++ C++/login/otrologin.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
+class login{
+     public:
+      string UserName;
+      string password;
+    bool Verificacion(string a, bool b);
+};
+
+bool login::Verificacion(string a, bool b)
+{
+    if (a.length() >= 8)
+    {
+        cout<<"su contraseña cumple con los requisitos de seguridad \n";
+        b = true;
+    }else{
+        cout<<"su contraseña no cumple los requistos de seguridad \n";
+        b = false;
+    }
+    return b;
+}
 
 int main(int argc, char const *argv[])
 {
-    cout<<"hello world";
+    login myObj;
+    bool verificacion;
+    bool entrada = true;
+    
+    while (entrada)
+    {
+        cout<<"por favor deme su usuario"<<"\n";
+    cin>>myObj.UserName;
+
+    cout<<"deme su contraseña\n";
+    cin>>myObj.password;
+
+    //verificacion de contraseña
+    verificacion = myObj.Verificacion(myObj.password, false);
+
+            if (verificacion == false)
+            {
+                continue;
+            }else{
+                break;
+            }
+    }
+        cout<<"enhorabuena ya iniciaste seccion \n";
     return 0;
 }
