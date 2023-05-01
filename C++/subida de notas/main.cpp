0f0be71051f8c7c66455b0ce091f92a0887620c9
Clon5454 <clon5454@outlook.com>
2023-05-01 17:23:59 -0500

    creando la base del codigo

--- C++/subida de notas/main.cpp
+++ C++/subida de notas/main.cpp
@@ -3,8 +3,42 @@
 
 using namespace std;
 
+class Login
+{
+private:
+    string name;
+    string password;
+
+public:
+    Login(string Username, string UserPassword);
+};
+
+Login::Login(string Username, string UserPassword)
+{
+    name = Username;
+    password = UserPassword;
+}
+
 int main(int argc, char const *argv[])
 {
+    bool accept;       // iniciador del while
+    string interacion; // conversion de bool a string
+    cout << "bienvenido usuario se debe resgistrar, escriba acepto\n";
+    cin >> interacion;
+
+    if (interacion == "acepto")
+    {
+        accept = true;
+    }
+    else
+    {
+        accept = false;
+    }
+
+    while (accept)
+    {
+        /* code */
+    }
 
     return 0;
 }
