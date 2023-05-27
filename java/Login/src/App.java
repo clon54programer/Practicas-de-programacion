3574cae5036ccea17c8c6937c158235dd00389b1
Clon54 <clon5454@outlook.com>
2023-05-27 16:58:15 -0500

    creando las primera interacion del usuario

--- java/Login/src/App.java
+++ java/Login/src/App.java
@@ -2,51 +2,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        //voy a crear un login
-        
-        login mLogin = new login();
-        Scanner input = new Scanner(System.in);
-        boolean confirmacion = true;
 
-        while(confirmacion){
+        Scanner Input = new Scanner(System.in);
+        // varibles
+        String name;// recibira el nombre del usuario
+        String Contraseña;// recibira la contraseña de el usuario
 
-            System.out.println("deme su Username y su contraseña");
-            System.out.println("primero su Usuario");
-            mLogin.UserName = input.nextLine();
-            System.out.println(mLogin.UserName);
+        System.out.println("bienvenido usuario\n por favor ingrese usario y contraseña");
+        System.out.println("ingrese su nombre de usuario");
+        name = Input.nextLine();
+        System.out.println("ingrese su contraseña");
+        Contraseña = Input.nextLine();
 
-            System.out.println("deme su contraseña");
-            mLogin.password = input.nextLine();
-            System.out.println(mLogin.password);
-            //para ver si la logiado fue existosa
-            
-            //esto es para ver si relleno los campos
-            /*if(mLogin.UserName != null && mLogin.password != null){
-                System.out.println("falta un dato o dos de rellenar");
-                continue;
-            }*/
-                System.out.println("Gracias por dar los datos \n  Espere ver si cumplen los requisitos de seguridad \n");
-                
-            
-
-            mLogin.PasswordSecurity();
-            if(mLogin.confi == true){
-                System.out.println("se alogiado correctamente");
-                break;
-            }
-
-        }
-
-
-    }
-    public void Request(){
-        login request = new login();
-        if(request.password.length() >= 8){
-            System.out.println("su contraseña cumple los requisitos del seguridad");
-            request.confi = true;
-        }else{
-            System.out.println("su contraseña no cumple lso requisitos de seguridad");
-            request.confi = false;
-        }
+        // constantes
     }
 }
