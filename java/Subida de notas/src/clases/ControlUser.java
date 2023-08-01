24bedde78784a0ad359a07a4f3ff4522b141b8aa
Clon54 <clon5454@outlook.com>
2023-08-01 15:26:47 -0500

    dando funicones al metodo accion del usuario

--- java/Subida de notas/src/clases/ControlUser.java
+++ java/Subida de notas/src/clases/ControlUser.java
@@ -5,8 +5,12 @@ public class ControlUser {
     final private String ver = "ver";
     final private String actualizar = "actualizar";
 
+    public boolean iniciar;
+
     /**
-     * @param elecion es la ruta a seguri del programa
+     * este metodo verifica que el usuario alla escrito bien las rutas
+     * 
+     * @param elecion esta variable es la que recibe la funcion
      */
     void Accion_del_Usuario(String elecion) {
 
@@ -14,6 +18,8 @@ public class ControlUser {
         if (elecion != ver || elecion != actualizar) {
             System.out.println("escribiste mal una letra");
             System.exit(-1);
+        } else {
+            iniciar = true;
         }
 
     }
