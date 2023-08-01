be2a1ae79167be26e0d24efe6cd99890a73af5b4
Clon54 <clon5454@outlook.com>
2023-08-01 15:01:40 -0500

    renombrando las cosas

--- java/Subida de notas/src/App.java
+++ java/Subida de notas/src/App.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // objetsos
         Datos myObj = new Datos();
         Scanner input = new Scanner(System.in);
+        // objetos de clases
+
         // estudiantes
         myObj.estudiantes[0] = "Guillermo";
         myObj.estudiantes[1] = "juan";
