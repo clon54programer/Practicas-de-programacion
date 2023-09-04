fa5a979d6e500ba36bf35ee64392ae9d890d2169
Clon54 <clon5454@outlook.com>
2023-09-03 20:36:27 -0500

    reognarinzado programa

--- C#/Subida de notas/Program.cs
+++ C#/Subida de notas/Program.cs
@@ -7,20 +7,22 @@ namespace Program
 
     class SubidaDeNotas
     {
-        void Opciones(int opcion)
+
+
+        /// inicializmaos la hashTtableS
+        static Hashtable studentsTable = new Hashtable(); // las hash Table
+
+        static Grado_11 Student1 = new Grado_11(1, "Guillermo");
+        static Grado_11 Student2 = new Grado_11(2, "juan");
+        static Grado_11 Student3 = new Grado_11(3, "david");
+        static Grado_11 Student4 = new Grado_11(4, "juanjo");
+        static Grado_11 Student5 = new Grado_11(5, "agustin");
+        /// <summary>
+        /// este metodo se encargara de buscar todo en la clase principal
+        /// </summary>
+        static void inicio()
         {
 
-        }
-        static void Main()
-        {
-            /// inicializmaos la hashTtableS
-            Hashtable studentsTable = new Hashtable(); // las hash Table
-
-            Grado_11 Student1 = new Grado_11(1, "Guillermo");
-            Grado_11 Student2 = new Grado_11(2, "juan");
-            Grado_11 Student3 = new Grado_11(3, "david");
-            Grado_11 Student4 = new Grado_11(4, "juanjo");
-            Grado_11 Student5 = new Grado_11(5, "agustin");
 
             //delcamramos la tabla con sus claves y valores
             studentsTable.Add(Student1.ID, Student1);
@@ -28,13 +30,24 @@ namespace Program
             studentsTable.Add(Student3.ID, Student3);
             studentsTable.Add(Student4.ID, Student4);
             studentsTable.Add(Student5.ID, Student5);
+        }
+        static void Opciones(int opcion)
+        {
+
+        }
+        static void Main()
+        {
+            inicio();
+
 
             /// este codigo sirve para buscar a un estudiante en especifico
-            ///Grado_11 storeStudents1 = (Grado_11)studentsTable[Student1.ID];
+            Grado_11 storeStudents1 = (Grado_11)studentsTable[Student1.ID];
 
 
             Console.WriteLine("ingrese unas de las siguientes opciones\n" + "1. si quiere buscar a un estudiante es especifico\n" + "2.si quieres observar a todo los estudiantes\n" + "3.si quieres editar la nota de algun estudiante");
+            int input = Convert.ToInt32(Console.ReadLine());
 
+            Opciones(input);
 
 
         }
