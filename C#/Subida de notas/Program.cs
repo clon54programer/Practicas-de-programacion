22106b18adbf5626dc5fa01d2d5aae46b5e91ece
Clon54 <clon5454@outlook.com>
2023-09-03 20:03:02 -0500

    Poo

--- C#/Subida de notas/Program.cs
+++ C#/Subida de notas/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace Program
+{
+
+    class SubidaDeNotas
+    {
+        static void main()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
